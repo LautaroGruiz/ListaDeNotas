a7cf632c3116cf8aba33d759d16ef92f5cb6b053
Lautaro Gonzalez Ruiz <lautawork@gmail.com>
2023-11-27 22:43:47 -0300

    Proyecto terminado

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,12 +1,13 @@
-import "../Styles/App.css";
-import man from "../Image//PersonaEscritorio.png";
-import TodoCounter from "./TodoCounter";
-import { useState } from "react";
-import TaskItem from "./TaskItem";
+import "../Styles/App.css"
+import man from "../Image/PersonaEscritorio.png";
+import  { useEffect, useState } from 'react';
+import TaskItem from './TaskItem';
 
 function App() {
-  const [addedTask, setAddedTask] = useState("");
-  const [tasks, setTasks] = useState([]); // Estado para almacenar las tareas
+  const [addedTask, setAddedTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+  const [currentTasks, setCurrentTasks] = useState([]);
+  const [searchString, setSearchString] = useState('');
 
   const handleTaskChange = (e) => {
     e.preventDefault();
@@ -14,50 +15,77 @@ function App() {
   };
 
   const addTask = () => {
-    if (addedTask.trim() !== "") {
-      // Verifica que la tarea no esté vacía
-      setTasks([...tasks, addedTask]); // Agrega la tarea al estado de tareas
-      setAddedTask(""); // Limpia el input
+    if (addedTask.trim() !== '') {
+      const updatedTasks = [...tasks, addedTask];
+      setTasks(updatedTasks);
+      setAddedTask('');
+      localStorage.setItem('tasks', JSON.stringify(updatedTasks));
     }
   };
+
+  const deleteTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+    localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+  };
+
+  const updateCurrentTasks = (tasksArray, filter) => {
+    const filteredTasks = tasksArray.filter(
+      (task) =>
+        task.toLowerCase().includes(filter.toLowerCase())
+    );
+    setCurrentTasks(filteredTasks);
+  };
+
+  const handleChangeSearchString = (e) => {
+    setSearchString(e.target.value);
+  };
+
+  useEffect(() => {
+    const storedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    setTasks(storedTasks);
+    updateCurrentTasks(storedTasks, searchString);
+  }, [searchString]);
+
+  useEffect(() => {
+    updateCurrentTasks(tasks, searchString);
+  }, [searchString, tasks]);
+
   return (
     <>
       <main className="mainApp">
         <section className="contenedorCrearTasks">
-          <h2 className="CreateNewTask">Create new task</h2>
+          <h2 className="CreateNewTask">Crea una nota</h2>
           <div className="cajaInputAgregarTask">
-            <label className="tittleTaskName">Task name</label>
+            <label className="tittleTaskName">Nombre de la nota</label>
             <input
-              placeholder="Add the task"
+              placeholder="Escribe aqui"
               className="AddTaksInput"
               value={addedTask}
               onChange={handleTaskChange}
             />
           </div>
           <button onClick={addTask} className="buttonCreateTask">
-            Create task
+            Crear nota
           </button>
           <img className="imageMen" src={man} alt="Man adding a task" />
         </section>
         <section className="contenedorTasks">
-          <h1 className="tittleYourTasks">Your tasks</h1>
-          <TodoCounter />
+          <h1 className="tittleYourTasks">Tus notas</h1>
+          <h3 className="completeTittle">Aqui veras las notas que agregues</h3>
           <div className="contenedorInput">
             <input
               className="inputSearchTasks"
-              type="search"
-              onChange={(event) => {
-                console.log(event.target.value);
-              }}
-              name=""
-              id=""
+              type="text"
+              onChange={handleChangeSearchString}
+              value={searchString}
             />
-            <label>Search</label>
+            <label>Buscar</label>
           </div>
           <div className="boxComponentTasks">
-            {/* Mapea las tareas y crea un componente TaskItem para cada una */}
-            {tasks.map((taskText, index) => (
-              <TaskItem key={index} taskText={taskText} />
+            {currentTasks.map((taskText, index) => (
+              <TaskItem key={index} taskText={taskText} onDelete={() => deleteTask(index)} />
             ))}
           </div>
         </section>
